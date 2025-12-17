a67bab850fa4aba3bc64d65f729673ff38863a14
Sandesh <sandeshchy2059@gmail.com>
2025-12-17 13:07:35 +0545

    learning about different operators in python

---
+++ operators.py
@@ -0,0 +1,80 @@
+# #arithmetic operators
+# # addition
+# a = 10
+# b = 15
+# print(a + b)
+
+# # subtraction
+
+# print(a - b)
+
+# # multiplication
+
+# print(a * b)
+
+# #division
+
+# print(a / b)
+# # print(a / 0)
+
+# #exponent
+
+# print(a ** b)
+
+# # modulus
+
+# print(a % b)
+
+# #floor division
+
+# print(a // b)
+
+
+# relational operatiors
+
+# a = 33
+# b = 44
+
+# if a == b:
+#     print("same number")
+# elif a > b:
+#     print("a is greater")
+# else:
+#     print("b is greater")
+
+# a = True
+# b = False
+
+# c = not a
+# print(c)
+
+
+
+# task 1
+
+listing = []
+seting = {}
+
+if listing and seting:
+    print("True")
+else:
+    print("False")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
