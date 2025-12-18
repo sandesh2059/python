f67ad972cab7c04b80f187b823682a0e9b2838d3
Sandesh <sandeshchy2059@gmail.com>
2025-12-18 12:50:54 +0545

    assignment 3 added

---
+++ assignment3.py
@@ -0,0 +1,45 @@
+# # task1
+# student_scores = {'sandesh':[100, 99, 98], 'flins':[89, 88, 90], 'columbina':[98, 77, 99]}
+# marks = student_scores['sandesh']
+# average = sum(marks) / len(marks)
+# print("the average marks of sandesh is: ", average)
+# if average >= 60:
+#     print("True")
+# else:
+#     print("False")
+
+
+
+
+
+# task 2
+
+# stock = {"apples": 10, "bananas": 2, "oranges": 0}
+# print("pears" in stock)   #to check if there is a key named pears in the dictionary
+
+# # Use a logical operator to check if "bananas" are in stock and if the quantity is less than 5.
+# # If both are true, print "Time to restock bananas!".
+
+# if ("bananas" in stock) and stock["bananas"] < 5:
+#     print("True")
+#     print("time to restock bananas")
+# else:
+#     print("False")
+
+
+
+# task 3
+# users = {"admin": "1234"}
+# print(users)
+# users["teacher"] = "password789"
+# print(users)
+
+# input_user = "teacher"
+# input_pass = "password789"
+
+# if input_user in users and users[input_user] == input_pass:
+#     print("access granted")
+# else:
+#     print("access not granted")
+
+
