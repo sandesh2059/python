747d2e1d9da7e7f20ef3d67e039073fc3fbbb5f3
Sandesh <sandeshchy2059@gmail.com>
2025-12-19 22:19:05 +0545

    total digit count function added

--- practise/day3_functions/number_analyzer.py
+++ practise/day3_functions/number_analyzer.py
@@ -11,3 +11,13 @@ def palindrome(n):
         print("Palindrome: YES")
     else:
         print("Palindrome: NO")
+
+
+
+
+def total_digits(n):
+    count = 0
+    while n > 0:
+        n //= 10
+        count += 1
+    return count
