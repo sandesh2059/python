6f3d83f72de0c75c7154f61594cd69158d9db4bd
Sandesh <sandeshchy2059@gmail.com>
2025-12-19 22:19:35 +0545

    sum of digits function added

--- practise/day3_functions/number_analyzer.py
+++ practise/day3_functions/number_analyzer.py
@@ -21,3 +21,12 @@ def total_digits(n):
         n //= 10
         count += 1
     return count
+
+
+
+def total_sum(n):
+    add = 0
+    while n > 0:
+        add += n % 10
+        n //= 10
+    return add
