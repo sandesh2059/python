ce6a6fc1c818169b27bb6e25d7ff97b574f23340
Sandesh <sandeshchy2059@gmail.com>
2025-12-22 20:22:02 +0545

    learning to create a object

--- practise/day4_oops/day4.py
+++ practise/day4_oops/day4.py
@@ -1,7 +1,11 @@
 class students:
-    college_name: ACHS
+    college_name: 'ACHS'
 
     def __init__(self, name, rollno):
         self.name = name
         self.rollno = rollno
-        
+
+
+s1 = students("sandesh", 23081019)
+print(s1.name, s1.rollno)
+
