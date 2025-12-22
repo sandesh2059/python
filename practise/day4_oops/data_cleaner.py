2fc5e3a9ffef2146db5827793a43a74bc91944bc
Sandesh <sandeshchy2059@gmail.com>
2025-12-22 21:28:30 +0545

    converted the data cleaner file into oop file

--- practise/day4_oops/data_cleaner.py
+++ practise/day4_oops/data_cleaner.py
@@ -1,10 +1,37 @@
-name = ['Jaaannne', 'Maaarrk', 'Alleeexx']
-result = []
+# name = ['Jaaannne', 'Maaarrk', 'Alleeexx']
+# result = []
 
-for word in name:
-    unique = ''
-    for ch in word:
-        if ch not in unique:
-            unique += ch
-    result.append(unique)
-print(result)
+# for word in name:
+#     unique = ''
+#     for ch in word:
+#         if ch not in unique:
+#             unique += ch
+#     result.append(unique)
+# print(result)
+
+
+
+class data_cleaner:
+
+    def __init__(self, names):
+        self.names = names
+
+    def remove_duplicates(self):
+        result = []
+
+        for word in self.names:
+            unique = ''
+            for ch in word:
+                if ch not in unique:
+                    unique += ch
+            result.append(unique)
+        return result
+    
+    def show_updated_list(self):
+        cleaned = self.remove_duplicates()
+        print(cleaned)
+    
+names = ['Jaaannne', 'Maaarrk', 'Alleeexx']
+
+obj1 = data_cleaner(names)
+obj1.show_updated_list()
