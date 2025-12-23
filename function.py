a370431004c3f08bd59e6fd0450abd55554b7b6a
Sandesh <sandeshchy2059@gmail.com>
2025-12-23 12:28:06 +0545

    function topic started at class

---
+++ function.py
@@ -0,0 +1,72 @@
+# #  to find min max using function
+
+
+# def minmax():
+#     listing = []
+
+#     largest = float('-inf')
+#     smallest = float('inf')
+
+#     n = int(input("Enter how many elements in a list do you want: "))
+
+#     for i in range(0, n):
+#         element = int(input("enter the elements: "))
+#         listing.append(element)
+
+#         if listing[i] > largest:
+#             largest = listing[i]
+#         if listing[i] < smallest:
+#             smallest = listing[i]
+        
+#     print("the smallest number in the list is: ", smallest)
+#     print("the largest number in the list is: ", largest)
+
+# minmax()
+
+
+
+
+# to reverse a number
+
+# def reverse(n):
+#     rev = 0
+#     while n > 0:
+
+#         rev = rev * 10 + (n % 10)
+#         n //= 10
+    
+#     return rev
+# num = int(input("enter a number: "))
+# reverse(num)
+# print("the reversed number is: ", reverse(num))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+marks = [12, 33, 12, 44, 11]
+
+total = sum(marks)
+
+new_marks = int(input("enter a new marks: "))
+
+marks.append(new_marks)
+
+for i in range(len(marks))
+
+
+
+
+
