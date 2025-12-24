1be3da2b581d83f3be22f63ef422ed8ef0f80827
Sandesh <sandeshchy2059@gmail.com>
2025-12-24 19:37:07 +0545

    fixed errors

--- practise/day6_oops_continue/day6.py
+++ practise/day6_oops_continue/day6.py
@@ -12,26 +12,45 @@
 # Menu-driven program.
 
 class BankAccount():
-    account_holder = 'sandesh'
-    balance = 6000
-    def __init__(self, account_holder, balance, amount):
+    name = ['sandesh', 6000]
+    def __init__(self, account_holder, balance):
         self.account_holder = account_holder
         self.balance = balance
-        self.amount = amount
 
     
     def check_balance(self):
         return self.balance
     
 
-    def deposite(self):
-        amount = self.amount
-        balance = self.balance
-        amount = int(input("Enter amount to deposite: "))
-        balance = balance + amount
-        return balance
+    def deposite(self, amount):
+        if amount < 1:
+            print("invalid amount")
+        self.balance = self.balance + amount
+        return self.balance
     
-    def withdraw(self):
-        self.amount = int(input("Enter amount to withdraw: "))
-        self.balance = self.balance - self.amount
-        print(f"({self.amount} was withdrawn you new balance is {check_balance()})")
+    def withdraw(self, amount):
+        if self.balance < amount:
+            print("insufficient balance")
+        self.balance = self.balance - amount
+        return self.balance
+        
+    
+
+def menu():
+    name = input("enter account holder name: ")
+    balance = int(input("enter balance: "))
+    account = BankAccount(name, balance)
+    while True:
+        print("welcome to our bank")
+        print("enter 1 for checking balance")
+        print("enter 2 for depositing balance")
+        print("enter 3 for withdrawing balance")
+        print("enter 4 for exit")
+
+        choice = int(input("enter your choice: "))
+
+        if choice == 1:
+            print(f"your balance is ", account.check_balance())
+
+        
+menu()
