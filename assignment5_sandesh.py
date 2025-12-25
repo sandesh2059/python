dc20281a62fe91534db43be8802b670eaa7e13de
Sandesh <sandeshchy2059@gmail.com>
2025-12-25 18:04:43 +0545

    recursive palindrome function added

---
+++ assignment5_sandesh.py
@@ -0,0 +1,9 @@
+def recurisve_palindrome(word):
+    if len(word) <= 1:
+        return True
+    
+    if word[0] != word[-1]:
+        return False
+    
+    return recurisve_palindrome(word[1:-1])
+
