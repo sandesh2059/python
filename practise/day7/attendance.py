e115a3c9a7776be0987827ffcc312e620160a3d3
Sandesh <sandeshchy2059@gmail.com>
2025-12-26 20:35:14 +0545

    mark attendacne program added

--- practise/day7/attendance.py
+++ practise/day7/attendance.py
@@ -8,3 +8,20 @@ def add_student(students):
         "present": False
     })
     print("Student added successfully")
+def mark_attendance(students):
+    if not students:
+        print("No students available")
+        return
+
+    for i, s in enumerate(students, start=1):
+        print(f"{i}. {s['name']}")
+
+    choice = int(input("Select student number: "))
+    status = input("Mark Present (P) or Absent (A): ").upper()
+
+    if status == 'P':
+        students[choice - 1]["present"] = True
+    else:
+        students[choice - 1]["present"] = False
+
+    print("Attendance updated")
