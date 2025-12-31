c53ac91cb87d373bb0e0c2ef5610fdb2020cb310
Sandesh <sandeshchy2059@gmail.com>
2025-12-31 19:25:17 +0545

    grade method is created

--- 1.py
+++ 1.py
@@ -7,3 +7,13 @@ class Student:
         return sum(self.marks)
     def percentage(self):
         return self.total_marks() / len(self.marks)
+    def grade(self):
+        percent = self.percentage()
+        if percent >= 90:
+            return "A"
+        elif percent >= 75:
+            return "B"
+        elif percent >= 60:
+            return "C"
+        else:
+            return "Fail"
