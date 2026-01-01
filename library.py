213e228e8a40bb2ede00e3748cabbdcafd7769ef
Sandesh <sandeshchy2059@gmail.com>
2026-01-01 13:14:08 +0545

    show book method added

---
+++ library.py
@@ -0,0 +1,10 @@
+books = ['science', 'math', 'social', 'computer', 'english', 'nepali']
+class library():
+    def __init__(self, bookName, studentName):
+        self.bookName = bookName
+        self.studentName = studentName
+    
+    def showBooks():
+        return books
+print(f"the list of books are: {library.showBooks()}")
+
