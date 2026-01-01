f5bcfcbb0e9966667ebbbfa5e5655f83bfa3fd30
Sandesh <sandeshchy2059@gmail.com>
2026-01-01 14:43:24 +0545

    add marks method added

---
+++ practise/day10_higher_order_function/studentmanagement.py
@@ -0,0 +1,13 @@
+class student():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.marks = {}
+    
+    def addMarks(self, subject , mark):
+        self.marks.append[subject] = mark
+    
+    
+    
+
+        
