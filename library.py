782a6dfd98d5556a4237db7b67e7d8f58e58b19a
Sandesh <sandeshchy2059@gmail.com>
2026-01-01 13:35:14 +0545

    add student method added

--- library.py
+++ library.py
@@ -1,10 +1,21 @@
 books = ['science', 'math', 'social', 'computer', 'english', 'nepali']
+student = []
 class library():
-    def __init__(self, bookName, studentName):
-        self.bookName = bookName
+    def __init__(self, studentName):
         self.studentName = studentName
     
+    @staticmethod
     def showBooks():
         return books
+    
+    
+    def addStudent(self):
+        student.append(self.studentName)
+        return student
+
 print(f"the list of books are: {library.showBooks()}")
+
+
+addstudent1 = library(studentName = input("enter the name of student: "))
+print(f"students are: {addstudent1.addStudent()}")
 
