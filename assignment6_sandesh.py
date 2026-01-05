eff007c89f05fac88d2a1c46a59f2e42ab19e91e
Sandesh <sandeshchy2059@gmail.com>
2026-01-05 19:54:01 +0545

    deposit and withdraw method created

--- assignment6_sandesh.py
+++ assignment6_sandesh.py
@@ -6,5 +6,16 @@ class Wallet:
     def check_balance(self):
         return self.__balance
     
+    def deposit(self, amount):
+        self.__balance += amount
+        return self.__balance
+    
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.balance -= amount
+            return f"{amount} is withdrawn from {self.__ownerName}'s account\nyour new balance is {self.__balance}"
+        else:
+            return f"not enough balance"
+    
     
 
