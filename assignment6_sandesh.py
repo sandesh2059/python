643889dded2df811db6b93565ba882353d37a989
Sandesh <sandeshchy2059@gmail.com>
2026-01-05 19:51:08 +0545

    check balance method is created

--- assignment6_sandesh.py
+++ assignment6_sandesh.py
@@ -1,5 +1,10 @@
 class Wallet:
-    def __init__(self, ownerName, balance):
+    def __init__(self, ownerName, balance = 0):
         self.__balance = float(balance)
         self.__ownerName = ownerName
+    
+    def check_balance(self):
+        return self.__balance
+    
+    
 
