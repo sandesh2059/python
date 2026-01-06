894f7e6d7878ecf2cc55809c4b293470538cfefb
Sandesh <sandeshchy2059@gmail.com>
2026-01-06 20:37:30 +0545

    just practicing

---
+++ practise/day13_inheritance/day13.py
@@ -0,0 +1,10 @@
+class Account:
+    def __init__(self, acc_number, balance):
+        self.acc_number = acc_number
+        self.balance = balance
+    
+    def check_balance(self):
+        return self.balance
+
+
+        
