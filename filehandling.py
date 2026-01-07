6bc585ae464ef163ed67877549daca6b097236b0
Sandesh <sandeshchy2059@gmail.com>
2026-01-07 11:07:27 +0545

    just practicing file handling

--- filehandling.py
+++ filehandling.py
@@ -1,2 +1,6 @@
-with open("hello.txt", 'a') as file:
-    file.write("starting the practise of  handlings files ........")
+# with open("hello.txt", 'a') as file:
+#     file.write("starting the practise of  handlings files ........")
+
+with open("hello.txt", 'r') as file:
+    content = file.read()
+    print(content)
