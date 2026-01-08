d6d1912c80e2688c88f78353cf59a5999322ada1
Sandesh <sandeshchy2059@gmail.com>
2026-01-08 18:33:15 +0545

    just practising the concept of polymorphism

--- polymorphism.py
+++ polymorphism.py
@@ -1,24 +1,31 @@
+# class Shape:
+#     def area(self):
+#         return "area"
+    
+# class Rectangle(Shape):
+#     def __init__(self, length, breadth):
+#         self.length = length
+#         self.breadth = breadth
+    
+#     def area(self):
+#         return self.length * self.breadth
+
+# class Square(Shape):
+#     def __init__(self, length):
+#         self.length = length
+    
+#     def area(self):
+#         return self.length * self.length
+
+
+# def area_of_shape(enter_shape):
+#     return enter_shape.area()
+# shape = Square(5)
+# print(area_of_shape(shape))
+
+
+
+
 class Shape:
     def area(self):
-        return "area"
-    
-class Rectangle(Shape):
-    def __init__(self, length, breadth):
-        self.length = length
-        self.breadth = breadth
-    
-    def area(self):
-        return self.length * self.breadth
-
-class Square(Shape):
-    def __init__(self, length):
-        self.length = length
-    
-    def area(self):
-        return self.length * self.length
-
-
-def area_of_shape(Shape):
-    return Shape.area()
-shape = Square(5)
-print(area_of_shape(shape))
+        print("Area of shape is undefined")
