e6551cc32a342d5ff1695cf70bde62544eff8409
Sandesh <sandeshchy2059@gmail.com>
2026-01-08 18:33:40 +0545

    just practising the concept of polymorphism

--- polymorphism.py
+++ polymorphism.py
@@ -33,3 +33,14 @@ class Rectangle(Shape):
     def __init__(self, length, breadth):
         self.length = length
         self.breadth = breadth
+    def area(self):
+        return self.length * self.breadth
+
+
+# Derived class 2
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
