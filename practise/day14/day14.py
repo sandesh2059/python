f01a007c6b034ca8620321338031ec14919b9a16
Sandesh <sandeshchy2059@gmail.com>
2026-01-11 20:07:34 +0545

    just practising oop

--- practise/day14/day14.py
+++ practise/day14/day14.py
@@ -3,3 +3,16 @@ class Library:
     def __init__(self):
         # Dictionary to store books
         self.books = {}
+    
+    def add_book(self):
+        book_id = input("Enter Book ID: ")
+        name = input("Enter Book Name: ")
+        author = input("Enter Author Name: ")
+
+        self.books[book_id] = {
+            "name": name,
+            "author": author,
+            "available": True
+        }
+
+        print("Book added successfully!")
