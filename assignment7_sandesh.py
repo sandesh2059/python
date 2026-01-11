e72cde0ecac513ef378f948d70761e50e0bdda8e
Sandesh <sandeshchy2059@gmail.com>
2026-01-11 20:02:44 +0545

    just practising oop

--- assignment7_sandesh.py
+++ assignment7_sandesh.py
@@ -1,18 +1,24 @@
-from abc import ABC , abstractmethod
+class Employee:
+    def __init__(self, name, basic_salary):
+        self.__name = name
+        self.__basic_salary = basic_salary
 
-class EWallet(ABC):
+    def get_name(self):
+        return self.__name
 
-    @abstractmethod
-    def check_balance(self):
-        pass
+    def get_salary(self):
+        return self.__calculate_salary()
 
-    @abstractmethod
-    def deposit(self):
-        pass
+    def __calculate_salary(self):
+        bonus = self.__basic_salary * 0.2
+        return self.__basic_salary + bonus
+class Manager(Employee):
+    pass
+class Developer(Employee):
+    pass
 
-    @abstractmethod
-    def withdraw(self):
-        pass
+emp1 = Manager("Sandesh", 50000)
+emp2 = Developer("Dynamo", 40000)
 
-
-class esewa
+print(f"Manager: {emp1.get_name()}, Salary: {emp1.get_salary()}")
+print(f"Developer: {emp2.get_name()}, Salary: {emp2.get_salary()}")    
