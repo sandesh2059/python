d7cc82f7e0f61fe62fb36345d8336cb58c422111
Sandesh <sandeshchy2059@gmail.com>
2026-01-13 19:44:42 +0545

    just practising oops

--- practise/day15/day15.py
+++ practise/day15/day15.py
@@ -3,3 +3,15 @@ class Stadium:
         self.name = name
         self.capacity = capacity
         self.events = {}
+
+    def add_event(self, event_id, event_name, date, price):
+        if event_id in self.events:
+            print("Event ID already exists.")
+            return
+        self.events[event_id] = {
+            "name": event_name,
+            "date": date,
+            "price": price,
+            "booked_seats": 0
+        }
+        print("Event added successfully.")
