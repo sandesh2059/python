8270cbaeab6af05842ee8031b8aaf10a796f194f
Sandesh <sandeshchy2059@gmail.com>
2026-01-14 21:24:36 +0545

    just practising oops

--- practise/day16/day16.py
+++ practise/day16/day16.py
@@ -7,3 +7,18 @@ class Book:
 
     def display(self):
         print(f"ID: {self.book_id} | {self.title} by {self.author} | Copies: {self.copies}")
+
+class Library:
+    def __init__(self):
+        self.books = {}
+
+    def add_book(self):
+        book_id = input("Enter Book ID: ")
+        if book_id in self.books:
+            print("Book already exists!")
+            return
+        title = input("Enter Title: ")
+        author = input("Enter Author: ")
+        copies = int(input("Enter Copies: "))
+        self.books[book_id] = Book(book_id, title, author, copies)
+        print("Book added successfully")
