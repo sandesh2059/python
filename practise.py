ecbff0a090fef1f4c18a02d0c8dfa3eb61bde000
Sandesh <sandeshchy2059@gmail.com>
2026-02-06 20:37:58 +0545

    practising python concepts

--- practise.py
+++ practise.py
@@ -31,3 +31,10 @@ def count_characters(text):
 
 def reverse_string(text):
     return text[::-1]
+
+def find_max(numbers):
+    max_num = numbers[0]
+    for num in numbers:
+        if num > max_num:
+            max_num = num
+    return max_num
