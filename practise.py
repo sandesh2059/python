45b6687aa74dd978876dadd80b1c759dd174edb1
Sandesh <sandeshchy2059@gmail.com>
2026-02-06 20:37:19 +0545

    practising python concepts

--- practise.py
+++ practise.py
@@ -2,3 +2,11 @@ integer_num = 10
 float_num = 10.5
 string_text = "Python"
 boolean_val = True
+
+numbers_list = [1, 2, 3, 4, 5]
+unique_numbers = {1, 2, 3, 3, 4}
+student_info = {
+    "name": "Sandesh",
+    "age": 22,
+    "course": "CS"
+}
