743d1a0d8f56ca64df0a57c835832dd73111b1c1
Sandesh <sandeshchy2059@gmail.com>
2026-02-06 20:37:40 +0545

    practising python concepts

--- practise.py
+++ practise.py
@@ -19,3 +19,12 @@ def separate_even_odd(numbers):
         else:
             odd.append(num)
     return even, odd
+
+def count_characters(text):
+    char_count = {}
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
