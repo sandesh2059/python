43492f43d15688de3ba53ee375c7c40cae64300a
Sandesh <sandeshchy2059@gmail.com>
2026-02-10 19:03:11 +0545

    merge two sorted list

--- practise.py
+++ practise.py
@@ -223,3 +223,21 @@ def second_largest(nums):
     return second
 
 print(second_largest([10, 5, 20, 8]))
+
+def merge_sorted(a, b):
+    i = j = 0
+    result = []
+
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
+            result.append(a[i])
+            i += 1
+        else:
+            result.append(b[j])
+            j += 1
+
+    result.extend(a[i:])
+    result.extend(b[j:])
+    return result
+
+print(merge_sorted([1,3,5], [2,4,6]))
