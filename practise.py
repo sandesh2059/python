7b088ba2afc7cfb3f48c6d9bb5bd370b6225fb1d
Sandesh <sandeshchy2059@gmail.com>
2026-02-10 19:03:54 +0545

    custom iterator

--- practise.py
+++ practise.py
@@ -258,3 +258,19 @@ def are_anagrams(s1, s2):
     return True
 
 print(are_anagrams("listen", "silent"))
+
+class CountDown:
+    def __init__(self, start):
+        self.current = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current <= 0:
+            raise StopIteration
+        self.current -= 1
+        return self.current + 1
+
+for num in CountDown(5):
+    print(num)
