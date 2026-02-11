4c264c0f3f9c66097583d7f0e6f4605f2d2e1fd6
Sandesh <sandeshchy2059@gmail.com>
2026-02-11 20:27:09 +0545

    2nd largest

--- new.py
+++ new.py
@@ -16,3 +16,17 @@ def reverse_string(s):
     return result
 
 print(reverse_string("sandesh"))
+
+def second_largest(nums):
+    first = second = float('-inf')
+    
+    for num in nums:
+        if num > first:
+            second = first
+            first = num
+        elif first > num > second:
+            second = num
+    
+    return second
+
+print(second_largest([10, 20, 4, 45, 99]))
