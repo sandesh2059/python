fa7c821e205d9e9b759cb0ca1251067fd6d47fb9
Sandesh <sandeshchy2059@gmail.com>
2026-02-14 21:03:23 +0545

    flatten

--- new.py
+++ new.py
@@ -191,3 +191,14 @@ def list_intersection(a, b):
     return list(set(a) & set(b))
 
 print(list_intersection([1,2,3,4], [3,4,5,6]))
+
+def flatten(lst):
+    result = []
+    for item in lst:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
+
+print(flatten([1, [2, 3], [4, [5, 6]]]))
