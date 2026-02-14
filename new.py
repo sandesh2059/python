6d6f22af24dc9f0a70c90da08fe97aa852c2dcaf
Sandesh <sandeshchy2059@gmail.com>
2026-02-14 21:03:53 +0545

    rotate list

--- new.py
+++ new.py
@@ -209,3 +209,9 @@ def longest_word(sentence):
     return max(words, key=len)
 
 print(longest_word("Python is very powerful language"))
+
+def rotate_list(lst, k):
+    k = k % len(lst)
+    return lst[-k:] + lst[:-k]
+
+print(rotate_list([1,2,3,4,5], 2))
