45c1204ec35343f80b788eafa4cbc41209a7c141
Sandesh <sandeshchy2059@gmail.com>
2026-02-14 21:02:52 +0545

    is prime

--- new.py
+++ new.py
@@ -176,3 +176,13 @@ def find_duplicates(lst):
     return list(duplicates)
 
 print(find_duplicates([1,2,3,4,2,5,1,6]))
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(29))
