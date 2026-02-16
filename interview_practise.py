32cc06c5b5dba5e1b0d3a82c5b140607a1478905
Sandesh <sandeshchy2059@gmail.com>
2026-02-16 20:44:56 +0545

    list comprehension

--- interview_practise.py
+++ interview_practise.py
@@ -1,10 +1,26 @@
-# is prime?
+# # is prime?
 
-def is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
-print(is_prime(num=int(input("enter a number: "))))
+# def is_prime(num):
+#     if num <= 1:
+#         return False
+#     for i in range(2, num):
+#         if num % i == 0:
+#             return False
+#     return True
+# print(is_prime(num=int(input("enter a number: "))))
+
+#if even square that number and put it in list
+
+
+# def list_comprehension(list):
+    
+    
+#     square = [n*n for n in a if n % 2 == 0]
+#     return square
+# a = [1, 2, 3, 4, 5, 6]
+# print(list_comprehension(a))
+
+
+num = [1, 2, 3, 4, 5]
+new_num = [n+10 for n in num ]
+print(new_num)
